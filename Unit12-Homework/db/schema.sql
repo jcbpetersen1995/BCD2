5375e4d3db5d062b952f0532bcb10aa0e1b2dd40
Jacob Petersen <jcb.petersen1995@gmail.com>
2020-04-02 14:40:36 -0600

    app works as designed

--- Unit12-Homework/db/schema.sql
+++ Unit12-Homework/db/schema.sql
@@ -1,6 +1,22 @@
 CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
+CREATE TABLE department
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
 CREATE TABLE employee (
     id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
@@ -10,17 +26,4 @@ CREATE TABLE employee (
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role (id)
 );
-CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department (id)
-);
-CREATE TABLE department
-(
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
-);
+
--- Unit12-Homework/db/seed.sql
+++ Unit12-Homework/db/seed.sql
@@ -1,8 +1,20 @@
 USE employee_trackerDB;
+
 INSERT INTO department (name) VALUES ("Sales");
 INSERT INTO department (name) VALUES ("Service");
 INSERT INTO department (name) VALUES ("Finance");
 INSERT INTO department (name) VALUES ("Parts");
+
+INSERT INTO role (title, salary, department_id) VALUES ("Service Advisor", 80000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Service Advisor", 100000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Service Manager", 150000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Assistant Service Manager", 140000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Salesperson", 90000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Salesperson", 70000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Parts Advisor", 50000, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("Finance Director", 120000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Parts Advisor", 50000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Jacob", "Petersen", 1, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Thea", "Hovet", 1, null);
@@ -15,13 +27,3 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Cam",
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Jason", "Thomas", 3, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Peter", "Le", 4, null);
 
-INSERT INTO role (title, salary, department_id) VALUES ("Service Advisor", 80000, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Service Advisor", 100000, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Service Manager", 150000, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Assistant Service Manager", 140000, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Salesperson", 90000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Salesperson", 70000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Salesperson", 80000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Parts Advisor", 50000, 4);
-INSERT INTO role (title, salary, department_id) VALUES ("Finance Director", 120000, 3);
-INSERT INTO role (title, salary, department_id) VALUES ("Parts Advisor", 50000, 4);
